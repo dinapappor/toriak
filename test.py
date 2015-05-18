6ad179e2a9c7003b17c045f3aae0046e0dba7ec7
Dina pappor <ndrsofua@gmail.com>
2015-05-18 12:30:08 +0200

    More functionality added.

--- test.py
+++ test.py
@@ -5,6 +5,12 @@ from tornado.ioloop import IOLoop
 from tornado import gen
 
 AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
+class BucketProperties(object):
+    def __init__(self,
+                 **kwargs):
+        #for k, v in kwargs:
+        #    setattr(self, k, v)
+        self.__dict__.update(kwargs)
 
 class Riak(object):
     def __init__(self,
@@ -18,26 +24,45 @@ class Riak(object):
             self._riak_url = riak_url
 
     @gen.coroutine
-    def _return_response(self, data):
+    def _return_response(self, data, key_type=None):
         data = ujson.loads(data.body)
-        if "buckets" in data:
-            data = data["buckets"]
-        elif "props" in data:
-            data = data["props"]
-        print data
+        if "buckets" or "keys" in key_type:
+            data = data[key_type]
+        elif "props" in key_type:
+            data = BucketProperties(**data[key_type])
+        elif not key_type:
+            print data
         raise gen.Return(data)
 
     @gen.coroutine
     def get_bucket_properties(self, bucket_name="_riak_client_test"):
-        print "in get_bucket_properties"
-        r = yield self._http_get(url="buckets/{0}/props".format(bucket_name))
+        r = yield self._http_get(
+            url="buckets/{0}/props".format(bucket_name),
+            key_type="props")
         raise gen.Return(r)
 
 
     @gen.coroutine
-    def _http_get(self, url):
-        request = HTTPRequest(url=self._riak_url + url,
-                              method="GET")
+    def _http_get(self, url, key_type):
+        request = HTTPRequest(
+            url=self._riak_url + url,
+            method="GET")
+        try:
+            response = yield self._client.fetch(request)
+        except Exception, e:
+            pass
+        raise gen.Return(self._return_response(response, key_type))
+
+    @gen.coroutine
+    def __http_put(self, bucket, key, data):
+        data = ujson.dumps(data)
+        request = HTTPRequest(
+            url="buckets/{0}/keys/{1}/".format(
+                bucket,
+                key),
+            method="PUT",
+            headers= {"Content-Type": "application/json"},
+            body=data)
         try:
             response = yield self._client.fetch(request)
         except Exception, e:
@@ -45,11 +70,25 @@ class Riak(object):
         raise gen.Return(self._return_response(response))
 
     @gen.coroutine
-    def list_buckets(self):
-        r = yield self._http_get(url="buckets?buckets=true")
+    def list_keys(self, bucket_name="riak_client_test"):
+        r = yield self._http_get(
+            url="buckets/{0}/keys?keys=true".format(bucket_name),
+            key_type="keys")
         raise gen.Return(r)
 
+    @gen.coroutine
+    def list_buckets(self):
+        r = yield self._http_get(
+            url="buckets?buckets=true",
+            key_type="buckets")
+        raise gen.Return(r)
+
+
 if __name__ == '__main__':
+    from __main__ import Riak
+    import timeit
+    from __main__ import IOLoop
     R = Riak(riak_url="http://172.16.0.105:8098/")
-    IOLoop.instance().run_sync(R.list_buckets)
     IOLoop.instance().run_sync(R.get_bucket_properties)
+    IOLoop.instance().run_sync(R.list_keys)
+    IOLoop.instance().run_sync(R.list_keys)
